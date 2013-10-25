0a5dfc9fefec09150807ab49028265ba74b78b92
Evgeniy Kuzmin <jekccs@gmail.com>
2013-10-25 16:32:15 +0300

    Behat common contexts

---
+++ Behat/BaseContext.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace LaMelle\Framework\Behat;
+use Behat\MinkExtension\Context\MinkContext;
+
+use Behat\Behat\Context\BehatContext;
+use Behat\Symfony2Extension\Context\KernelAwareInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+/**
+ * Base context class.
+ *
+ * @author Evgeniy Kuzmin <jekccs@gmail.com>
+ */
+abstract class BaseContext extends BehatContext implements KernelAwareInterface
+{
+    /**
+     * Kernel.
+     *
+     * @var KernelInterface
+     */
+    protected $kernel;
+
+    /**
+     * Sets HttpKernel instance.
+     * This method will be automatically called by Symfony2Extension ContextInitializer.
+     *
+     * @param KernelInterface $kernel
+     */
+    public function setKernel(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    protected function getEntityManager()
+    {
+        return $this->getService('doctrine.orm.entity_manager');
+    }
+
+    protected function generateUrl($route, array $params = array())
+    {
+        return $this->getRouter()->generate($route, $params);
+    }
+
+    protected function getRouter()
+    {
+        return $this->kernel->getContainer()->get('router');
+    }
+
+    protected function getService($id)
+    {
+        return $this->kernel->getContainer()->get($id);
+    }
+        
+}
---
+++ Behat/PagesContext.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace LaMelle\Framework\Behat;
+
+use Behat\Behat\Context\Step;
+
+/**
+ * Moving around page context.
+ *
+ * @authorEvgeniy Kuzmin <jekccs@gmail.com>
+ */
+class PagesContext extends BaseContext
+{   
+    /**
+     * @Then /^I should be on login page$/
+     */
+    public function iShouldBeOnLoginPage()
+    {
+        return $this->iShouldBeOnRoute('fos_user_security_login');
+    }
+
+    /**
+     * @Then /^I should be on registration page$/
+     */
+    public function iShouldBeOnRegistrationPage()
+    {
+        return $this->iShouldBeOnRoute('fos_user_registration_register');
+    }
+
+    private function iAmOnRoute($route, array $parameters = array())
+    {
+        return $this->iAmOn($this->generateUrl($route, $parameters));
+    }
+
+    private function iAmOn($path)
+    {
+        return new Step\Given(sprintf('I am on "%s"', $path));
+    }
+
+    private function iShouldBeOnRoute($route, array $parameters = array())
+    {
+        return $this->iShouldBeOn($this->generateUrl($route, $parameters));
+    }
+
+    private function iShouldBeOn($path)
+    {
+        return new Step\Given(sprintf('I should be on "%s"', $path));
+    }
+}
