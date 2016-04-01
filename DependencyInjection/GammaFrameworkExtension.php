da2825ec93d1449f317ef708d35a9ee40dc7b91b
Evgeniy Kuzmin <jekccs@gmail.com>
2016-04-01 16:40:57 +0300

    load service at bundle

---
+++ DependencyInjection/GammaFrameworkExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Gamma\Framework\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
+use Symfony\Component\DependencyInjection\Loader;
+
+class GammaFrameworkExtension extends ConfigurableExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function loadInternal(array $mergedConfig, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
---
+++ GammaFrameworkBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Gamma\Framework;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GammaFrameworkBundle extends Bundle
+{
+}
